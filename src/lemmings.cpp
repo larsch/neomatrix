bd0d1f8377da57fc99950f355dfd19a8e14f3adc
Lars Christensen <larsch@belunktum.dk>
2017-03-16 20:04:02 +0100

    Style

--- src/lemmings.cpp
+++ src/lemmings.cpp
@@ -46,7 +46,6 @@ void Lemmings::run()
   pixel* p = &pixels[7 * 24];
   for (int y = 0; y < 10; ++y) {
     for (int x = 0; x < 24; ++x) {
-
       int w = (x + INTERVAL * FRAMES - step) % INTERVAL;
       if (w < 6) {
         int bit = (y * 6 + w) * 2;
