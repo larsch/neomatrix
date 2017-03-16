70b5408e7f2d4e67194604a00e34e51867ed81e9
Lars Christensen <larsch@belunktum.dk>
2017-03-16 20:05:10 +0100

    Restyle .ino

--- restyle.sh
+++ restyle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-files=$(find src sim -name \*.cpp -o -name \*.hpp)
+files=$(find src sim -name \*.cpp -o -name \*.hpp -o -name \*.ino)
 perl0pie="perl -0 -p -i -e"
 $perl0pie "s/ +$//gm" $files
 $perl0pie "s/\n{2,}(^\s*})/\n\1/gm" $files
