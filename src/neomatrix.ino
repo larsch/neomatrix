0cb0674dbdff9e5a447551e550cff96965799391
Lars Christensen <larsch@belunktum.dk>
2017-07-10 23:41:47 +0200

    Echo ADC on TCP connection

--- src/neomatrix.ino
+++ src/neomatrix.ino
@@ -7,16 +7,35 @@
 
 #include "neomatrix.hpp"
 #include "wifi.hpp"
+#include "WiFiServer.h"
+#include "WiFiClient.h"
+#include "counter.hpp"
+
+WiFiServer server(1080);
+WiFiClient client;
+
+static Counter<1, 2> tick;
+static uint32_t last = 0;
 
 void setup()
 {
   Serial.begin(115200);
   wifi_init();
   neomatrix_init();
+  server.begin();
 }
 
 void loop()
 {
   wifi_run();
   neomatrix_run();
+
+  if (client) {
+    uint32_t now = tick();
+    if (now != last)
+      client.println(analogRead(A0));
+    last = now;
+  } else {
+    client = server.available();
+  }
 }
